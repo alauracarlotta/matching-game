d7f6c653fcc3fcc7ce6fa7bead28ce7f5e725801
alauracarlotta <alaurapythonista@gmail.com>
2025-06-24 21:21:08 -0300

    💄 Style: change semanthic tags

--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -2,10 +2,8 @@ import './style.css'
 
 const PlayerScore = (player) => {
     return /* html */ `
-        <div>
-            <p class="name-player press-start-2p-regular">
-                ${player}
-            </p>
+        <div class="name-player press-start-2p-regular">
+            Player ${player}
         </div>
     `
 };
