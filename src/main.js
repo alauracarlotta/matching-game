a13942e05f29ffaba67d78bad3d67bef70f5d2fb
alauracarlotta <alaurapythonista@gmail.com>
2025-06-24 21:18:24 -0300

    ✨ Feat: create BoardGame Object

--- src/main.js
+++ src/main.js
@@ -3,25 +3,24 @@ import "/src/styles/settings/colors.css";
 import "/src/styles/base/reset.css";
 import "/src/styles/elements/base.css";
 
+/* Objects */
+import BoardGame from "./objects/BoardGame";
+
 /* Components */
 import PlayerScore from "./components/PlayerScore";
-import CardGame from "./components/CardGame";
 
 const $root = document.querySelector("#root");
-const $htmlCardGame = CardGame();
-const $htmlPlayerScore = PlayerScore('Player');
+const $htmlBoardGame = BoardGame(6);
 
-for (let i = 0; i < 2; i++) {
-    $root.insertAdjacentHTML("beforeend", ($htmlPlayerScore + `${i+1}`))
+for (let i = 1; i <= 2; i++) {
+    $root.insertAdjacentHTML("beforeend", PlayerScore(i))
 }
 
-for (let i = 0; i < 6; i++) {
-    $root.insertAdjacentHTML("beforeend", ($htmlCardGame + `${i}`));
-}
+$root.insertAdjacentHTML("beforeend", ($htmlBoardGame))
 
 //WIP
 /* 
 [X] DESAFIO fácil: replicar o componente do card 6x
-intermediário criar o componente de nome de player com cada um tendo o seu nome
+[X] intermediário criar o componente de nome de player com cada um tendo o seu nome
 difícil criar o outro lado do cartão e quando clicar nele ele deve virar
 */
