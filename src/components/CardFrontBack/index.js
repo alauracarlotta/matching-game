bb7863e10a647982660aa7776eff1222f1a8a6e6
alauracarlotta <alaurapythonista@gmail.com>
2025-06-28 21:49:49 -0300

    💄 Style: add animate style CardFrontBack component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -4,11 +4,15 @@ import CardGame from '/src/components/CardGame';
 const CardFrontBack = () => {
     return /* html */ `
         <article class="card-front-back">
-            ${CardGame('joystick', 'Logo na cor branca de um joystick')}
-            ${CardGame(
-                'javascript',
-                'Logo da linguagem de programação Javascript em amarelo'
-            )}
+            <div class="card -front">
+                ${CardGame('joystick', 'Logo na cor branca de um joystick')}
+            </div>
+            <div class="card -back">
+                ${CardGame(
+                    'javascript',
+                    'Logo da linguagem de programação Javascript em amarelo'
+                )}
+            </div>
         </article>
     `;
 };
