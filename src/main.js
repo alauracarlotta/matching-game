c57f8a637bfae213188d3cdb207eb0caafcddf0d
alauracarlotta <alauracarlotta@gmail.com>
2025-06-30 19:26:15 -0300

    🚧 Wip: add challenges easy, medium and hard

--- src/main.js
+++ src/main.js
@@ -25,3 +25,12 @@ $root.insertAdjacentHTML(
 [X] DESAFIO intermediário: Criar o componente de nome de player com cada um tendo o seu nome
 [X] DESAFIO difícil: criar o outro lado do cartão e quando clicar nele ele deve virar
 */
+
+//
+/* 
+[ ] DESAFIO fácil: Criar componente de pontuação e vs (sem a marcação de pontos)
+[ ] DESAFIO intermediário: Criar o componente da seta que muda do player 1 para o player 2
+    Dica: Usar classes que mudarão no devtools do browser. 
+    Ex.: "se tem a classe player1, aponta para o player 1 e vice-versa"
+[ ] DESAFIO difícil: Assim que o usuário clicar em 2 cartões, a seta mudará para o player 2 e irá desvirar as cartas automaticamente.
+*/
