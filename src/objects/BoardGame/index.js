e05094d3d3f7b18addc222b2854b67dbff6e6059
alauracarlotta <alauracarlotta@gmail.com>
2025-07-06 16:43:35 -0300

    ✨ Feat: add toggle current player

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,7 +3,7 @@ import shuffledCards from './data.js';
 import CardFrontBack from '/src/components/CardFrontBack';
 
 const BoardGame = (amountCards) => {
-	window.boardGame = {};
+	/* window.boardGame = {};
 	let cardListBoardGameClicks = [];
 	window.boardGame.handleClick = (event) => {
 		console.log(event.target);
@@ -20,14 +20,45 @@ const BoardGame = (amountCards) => {
 				cardListBoardGameClicks = [];
 			}, 1000);
 		}
+	}; */
+
+	const flipAndHideCards = ($cardActive) => {
+		$cardActive.forEach((card) => {
+			card.classList.remove('-active');
+		});
 	};
+
+	const toggleCurrentPlayer = () => {
+		const $arrowPlayer = document.querySelector('.game-pointer');
+		const currentPlayer = $arrowPlayer.getAttribute('data-current-player');
+
+		$arrowPlayer.setAttribute(
+			'data-current-player',
+			currentPlayer == 1 ? 2 : 1
+		);
+	};
+	window.boardGame = {};
+	window.boardGame.handleClick = () => {
+		const $cardActive = document.querySelectorAll('.-active');
+
+		if ($cardActive.length === 2) {
+			setTimeout(() => {
+				flipAndHideCards($cardActive);
+			}, 1000);
+
+			setTimeout(() => {
+				toggleCurrentPlayer();
+			}, 1500);
+		}
+	};
+
 	const htmlCadsList = shuffledCards.map((card) =>
 		CardFrontBack(card.iconImg, card.imgAlt)
 	);
 	const $htmlContent = htmlCadsList.join('');
 
 	return /* html */ `
-        <section class="board-game" onclick="boardGame.handleClick(event)">
+        <section class="board-game" onclick="boardGame.handleClick()">
             <div class="cards-game">
                 ${$htmlContent}
             </div>
